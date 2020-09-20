0cbe152b65b20c3f86555107298f95e5c7804880
straightlight <32371528+straightlight@users.noreply.github.com>
2020-09-20 09:33:39 -0400

    Added integer on $page get request.

--- upload/admin/controller/marketplace/marketplace.php
+++ upload/admin/controller/marketplace/marketplace.php
@@ -979,7 +979,7 @@ class Marketplace extends \Opencart\System\Engine\Controller {
 		}
 
 		if (isset($this->request->get['page'])) {
-			$page = $this->request->get['page'];
+			$page = (int)$this->request->get['page'];
 		} else {
 			$page = 1;
 		}
